2493eee24a6ca3bceb491f803682d25a3988cc3b
leebwj <109943383+leebwj@users.noreply.github.com>
2024-12-13 22:12:01 -0500

    Add files via upload

---
+++ PixelDash/src/main/java/org/project/pixeldash/GroundObstacle.java
@@ -0,0 +1,23 @@
+package org.project.pixeldash;
+
+import java.awt.*;
+
+/**
+ * Extends GameObj class. Provide attributes and methods for game
+ * objects like position, size, movement, and boundaries. For Pixel Run mode
+ */
+
+public class GroundObstacle extends GameObj {
+
+    // Initialize new GroundObstacle object
+    public GroundObstacle(int px, int py, int width, int height, int vx, int courtWidth, int courtHeight) {
+        super(px, py, width, height, vx, 0, courtWidth, courtHeight);
+    }
+
+    // Overrides GameObj move method to update obstacle’s position based on its velocity value.
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.fillRect(px, py, width, height); // Represent ground obstacle as a black rectangle
+    }
+}
